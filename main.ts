2aae0b2d913b9b0cb24afc889872bc2b1bf34a73
CryptoGladi <dam.almaev@gmail.com>
2025-11-28 04:25:01 +0300

    Add try catch

--- main.ts
+++ main.ts
@@ -13,35 +13,40 @@ export default class MyPlugin extends Plugin {
 			id: 'get-metrics-to-json',
 			name: 'Get metrics to json',
 			callback: async () => {
-				console.time("getMetrics");
-				new Notice('Getting metrics...');
+				try {
+					console.time("getMetrics");
+					new Notice('Getting metrics...');
 
-				let files = this.app.vault.getMarkdownFiles();
+					let files = this.app.vault.getMarkdownFiles();
 
-				// @ts-ignore
-				let basePath = this.app.vault.adapter.basePath;
+					// @ts-ignore
+					let basePath = this.app.vault.adapter.basePath;
 
-				const promises = files.map(async (file) => {
-					let text = await this.app.vault.cachedRead(file);
-					let path = `${basePath}/${file.path}`;
+					const promises = files.map(async (file) => {
+						let text = await this.app.vault.cachedRead(file);
+						let path = `${basePath}/${file.path}`;
 
-					return new plugin.NoteInfo(text, path);
-				});
+						return new plugin.NoteInfo(text, path);
+					});
 
-				let textFromFiles = await Promise.all(promises);
-				let json_metrics = plugin.get_json_metrics(textFromFiles, basePath);
+					let textFromFiles = await Promise.all(promises);
+					let json_metrics = plugin.get_json_metrics(textFromFiles, basePath);
 
-				let file_metrics = this.app.vault.getRoot().children.find((d) => {
-					return d.path == "metrics.json";
-				});
+					let file_metrics = this.app.vault.getRoot().children.find((d) => {
+						return d.path == "metrics.json";
+					});
 
-				if (file_metrics instanceof TAbstractFile)
-					this.app.vault.delete(file_metrics);
+					if (file_metrics instanceof TAbstractFile)
+						this.app.vault.delete(file_metrics);
 
-				this.app.vault.create("metrics.json", json_metrics);
+					this.app.vault.create("metrics.json", json_metrics);
 
-				console.timeEnd("getMetrics");
-				new Notice('Done getting metrics!');
+					console.timeEnd("getMetrics");
+					new Notice('Done getting metrics!');
+				}
+				catch (error) {
+					new Notice(`Error: ${error}`);
+				}
 			}
 		});
 	}
